9e41b8ebafe750318cfb658a1006eea8e23743ae
selvinmedina <selvinmedina0016@gmail.com>
2020-04-03 15:47:49 -0600

    Haciendo post de login

---
+++ ApiPruebaFARSIMAN/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using ApiPruebaFARSIMAN.Models.ViewModels;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+
+namespace ApiPruebaFARSIMAN.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // GET: Login
+        public string Get()
+        {
+            return "hola";
+        }
+
+        [ResponseType(typeof(Login))]
+        public object Post(Login login)
+        {
+
+            return login;
+        }
+    }
+}
+
---
+++ ApiPruebaFARSIMAN/Models/ViewModels/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiPruebaFARSIMAN.Models.ViewModels
+{
+    public class Login
+    {
+        public string nombreUsuario { get; set; }
+        public string password { get; set; }
+    }
+}
